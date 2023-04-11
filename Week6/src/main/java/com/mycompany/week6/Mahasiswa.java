3d7109cb3b15b8d0958be0791edd3e0bc66eec2c
Ningtyy <129071512+Ningtyy@users.noreply.github.com>
2023-04-11 11:05:07 +0700

    Add files via upload

---
+++ Week6/src/main/java/com/mycompany/week6/Mahasiswa.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.week6;
+
+/**
+ *
+ * @author D2J-00
+ */
+public class Mahasiswa {
+    
+}
---
+++ Week6/src/main/java/com/mycompany/week6/Week6.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.week6;
+
+/**
+ *
+ * @author D2J-00
+ */
+public class Week6 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
